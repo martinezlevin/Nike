8550ea53e7657f3a834fc02576d6882f12c0734e
Guillermo Martinez Levin <gdmlevin@gmail.com>
2022-09-29 10:18:57 -0300

    price

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+let product = []
+let total = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let product = [];
+let total = 0;
+
+function add(product, price) {
+    console.log(product, price);
+    product.push(product);
+    total = total + price;
+    document.getElementById("checkout").innerHTML = 'Card $${total}'
+}
+
+function pay(product, price) {
+    Window.alert(products.join(", \n"));
+}*/
