a753d19d0b0feeb015c9ff55e26548bd591dcc75
Guillermo Martinez Levin <gdmlevin@gmail.com>
2022-10-01 23:35:12 -0300

    arreglos

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
-let product = []
-let total = []
+let cantidad = prompt("Ingresa la cantidad");
+let producto = prompt("Zapatilla");
+
+let total = cantidad + producto;
+
+alert("total")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,4 +64,33 @@ function add(product, price) {
 
 function pay(product, price) {
     Window.alert(products.join(", \n"));
+
+let producto1 = 400;
+let producto2 = 400;
+let producto3 = 400;
+let producto4 = 400;
+let producto5 = 400;
+let producto6 = 400;
+let producto7 = 400;
+let producto8 = 400;
+let cantidad = (carrito);
+
+let resultadoSuma1 = producto1 * cantidad;
+let resultadoSuma2 = producto2 * cantidad;
+let resultadoSuma3 = producto3 * cantidad;
+let resultadoSuma4 = producto4 * cantidad;
+let resultadoSuma5 = producto5 * cantidad;
+let resultadoSuma6 = producto6 * cantidad;
+let resultadoSuma7 = producto7 * cantidad;
+let resultadoSuma8 = producto8 * cantidad;
+let resultadoTotal = carrito;
+
+alert(resultadoTotal);
+
+
+
+
+
+    
+let nombreIngresado = prompt("Ingrese su nombre");
 }*/
